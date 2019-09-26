6248ea8d483f9c544796580d7ccf0e8aa6ab33be
Chris Allen <allenc5@rpi.edu>
2019-09-26 10:44:43 -0400

    Removed stop-words from queries, and presents results in order of relevance

--- search_word.py
+++ search_word.py
@@ -1,13 +1,26 @@
 import sys
 import redis
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 
 r = redis.Redis(host='localhost', port=6379)
+stop_words = set(stopwords.words('english'))
 
 def search(query):
     tokens = word_tokenize(query)
-    results = r.sinter(tokens)
+    filtered_words = []
+
+    for word in tokens:
+        if word not in stop_words:
+            filtered_words.append(word)
+
+    results1 = r.sinter(filtered_words)
+
+    results2 = r.sunion(filtered_words) - results1
+
+    results = list(results1) + list(results2)
+
     return results
 
 
